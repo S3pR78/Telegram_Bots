00d6eaab7cb815bab4183197f637f9b3f6a8433f
S3pR78 <s3moprojects@gmail.com>
2026-07-12 19:10:51 +0200

    connect start command to handler

--- Test_Bot/bot.py
+++ Test_Bot/bot.py
@@ -1,12 +1,29 @@
 import os
 
 from dotenv import load_dotenv
-
+from telegram import Update
+from telegram.ext import Application , CommandHandler
 load_dotenv()
 
 token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+
+application = Application.builder().token(token).build()
 
 if token:
     print("Token loaded successfully.")
 else:
     print("Failed to load token. Please check your .env file.")
+
+if application:
+    print("Application created successfully.")
+else:
+    print("Failed to create application. Please check your token and try again.")
+
+
+async def start(update: Update, context):
+    print("Start command received.")
+
+
+application.add_handler(CommandHandler("start", start))
+
