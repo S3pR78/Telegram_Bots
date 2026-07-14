171efa9de403fa5e719ebbe351ad347ab552809a
S3pR78 <s3moprojects@gmail.com>
2026-07-14 13:57:16 +0200

    create Telegram application

--- Study_Assistant_Bot/src/bot.py
+++ Study_Assistant_Bot/src/bot.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 
+from telegram.ext import Application
+
 load_dotenv()
 
 Token = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -10,3 +12,11 @@ if Token is None:
     print("Failed to load token. Please check your .env file.")
 else:
     print("Token loaded successfully.")
+
+
+application = Application.builder().token(Token).build()
+
+if application is None:
+    print("Failed to create application. Please check your token and try again.")
+else:
+    print("Application created successfully.")
